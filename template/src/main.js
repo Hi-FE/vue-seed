2288c4a915f00f087c2ad03a020e7db53cc0edd3
不一 <buuyii@hitour.cc>
2016-08-25 18:18:54 +0800

    update

--- template/src/main.js
+++ template/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import VueRouter from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+Vue.use(VueRouter){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+const router = new VueRouter({
+  history            : true,
+  saveScrollPosition : true,
+  root               : '/'
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+
+router.map({
+  '/' : {component : require('./views/home.vue')}
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+
+const App = Vue.extend(require('./App.vue')){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+router.start(App, 'body'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
