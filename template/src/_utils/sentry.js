b2bc0916c0715249cc8ff02e7174dda7f5b591bd
cyh <cyh@hitour.cc>
2018-07-24 20:09:55 +0800

    refactor(template): Sentry, 重构部分代码，特殊处理 errorHandler

--- template/src/_utils/sentry.js
+++ template/src/_utils/sentry.js
@@ -10,7 +10,13 @@ class Sentry {
   install(Vue) {
     const { dsn } = this
 
-    if (dsn) this.raven = Raven.config(dsn).addPlugin(RavenVue, Vue).install()
+    if (dsn) {
+      // 使用 sentry 官方插件
+      Raven.config(dsn).addPlugin(RavenVue, Vue).install()
+
+      // 重新处理 errorHandler，覆盖 sentry 插件已处理的 errorHandler，覆盖
+      this.dealVueErrHandler(Vue)
+    }
 
     return this
   }
@@ -22,6 +28,33 @@ class Sentry {
     else fn()
 
     return this
+  }
+
+  dealVueErrHandler(Vue) {
+    const errorHandler = Vue.config.errorHandler || function () {}
+
+    Vue.config.errorHandler = function (err, vm, info) {
+      // 控制台输出 error
+      console.error(err)
+
+      const data = {
+        routeName: vm.$route.name,
+        routePath: vm.$route.path
+      }
+
+      // 插入新参数
+      Raven.setExtraContext(data)
+
+      // 执行 RavenVue 实现的 errorHandler
+      errorHandler(err, vm, info)
+
+      // 删除新参数
+      const context = Raven.getContext()
+      const extra = { ...context.extra }
+      for (const key in extra) delete extra[key]
+      Raven.setExtraContext()
+      Raven.setExtraContext(extra)
+    }
   }
 }
 
