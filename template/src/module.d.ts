54256313913441eb3a05719065b1ad1a350232c1
cyh <cyh@hitour.cc>
2018-08-01 15:22:33 +0800

    fix(template): typescript
    - raven-js/plugins/vue 定义不受控
    - $http 属性缺少定义

--- template/src/module.d.ts
+++ template/src/module.d.ts
@@ -3,21 +3,17 @@ interface Window {
   _vds: any
 }
 
-declare module "*.vue" {
-  import Vue from "vue";
-
-  export default Vue;
-}
-
-declare module 'raven-js/plugins/vue' {
-  import Vue from 'vue';
-  import { RavenStatic } from 'raven-js';
-  function vuePlugin(raven: RavenStatic, vue: Vue): RavenStatic;
-  export = vuePlugin;
-}
-
 declare var process : {
   env: {
     NODE_ENV: string
   }
 }
+{{#if_in options "sentry"}}
+
+declare module 'raven-js/plugins/vue' {
+  import Vue from 'vue'
+  import { RavenStatic } from 'raven-js'
+  function vuePlugin(raven: RavenStatic, vue: Vue): RavenStatic;
+  export = vuePlugin;
+}
+{{/if_in}}
